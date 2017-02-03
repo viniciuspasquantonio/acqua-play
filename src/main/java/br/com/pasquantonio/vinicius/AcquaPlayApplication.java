7411e26e0b56c162efa96568e33d6e1ed5b54969
Vinicius Pasquantonio <vinicius.pasquantonio@talentfour.com.br>
2017-02-02 23:49:47 -0200

    initial spring boot struct

---
+++ src/main/java/br/com/pasquantonio/vinicius/AcquaPlayApplication.java
@@ -0,0 +1,12 @@
+package br.com.pasquantonio.vinicius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcquaPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcquaPlayApplication.class, args);
+	}
+}
